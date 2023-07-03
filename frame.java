597fc53bd438086478fc76c0697c65f39f1c92aa
Toasty <94804887+TheToasty@users.noreply.github.com>
2023-07-03 10:43:33 +0200

    Add files via upload

---
+++ frame.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class frame{
+    public JFrame frame;
+    public JLayeredPane pane;
+    
+    public frame(){
+        frame = new JFrame();
+        
+        pane = new JLayeredPane();
+    }
+}
---
+++ slot.java
@@ -0,0 +1,7 @@
+public class slot{
+    
+    
+    public slot(){
+        
+    }
+}
